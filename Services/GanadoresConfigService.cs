734bcc3b2e1003e2a04faa5e8c7eb9b405575546
Ruben <ruben@drrsystemas.com>
2026-03-09 22:38:30 -0300

    cambioss

--- Services/GanadoresConfigService.cs
+++ Services/GanadoresConfigService.cs
@@ -15,7 +15,7 @@ public class GanadoresConfigService : IGanadoresConfig
         new()
         {
             Nombre = "Lamera Laura",
-            ImagenUrl = $"{BasePath}/ganador1.jpg.jpeg",
+            ImagenUrl = "https://i.imgur.com/OK1juNr.jpeg",
             Cuota = "6",
             Localidad = "Jardin America",
             FechaAdjudicacion = "29-11-2025",
@@ -24,7 +24,7 @@ public class GanadoresConfigService : IGanadoresConfig
         new()
         {
             Nombre = "Liz Maribel",
-            ImagenUrl = $"{BasePath}/ganador2.jpg.jpeg",
+            ImagenUrl = "https://i.imgur.com/TIYx3Wk.jpeg",
             Cuota = "29",
             Localidad = "Puerto Piray",
             FechaAdjudicacion = "29-11-2025",
@@ -33,7 +33,7 @@ public class GanadoresConfigService : IGanadoresConfig
         new()
         {
             Nombre = "Liz Maribel",
-            ImagenUrl = $"{BasePath}/ganador3.jpg.jpeg",
+            ImagenUrl = "https://i.imgur.com/LUmGkwa.jpeg",
             Cuota = "29",
             Localidad = "Puerto Piray",
             FechaAdjudicacion = "29-11-2025",
@@ -42,7 +42,7 @@ public class GanadoresConfigService : IGanadoresConfig
         new()
         {
             Nombre = "Vazquez Marcelo",
-            ImagenUrl = $"{BasePath}/ganador4.jpg.jpeg",
+            ImagenUrl = "https://i.imgur.com/C2isk55.jpeg",
             Cuota = "4",
             Localidad = "Irigoyen",
             FechaAdjudicacion = "29-11-2025",
